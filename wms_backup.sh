5ef1ed0a4259419418eb02584469a94d17cb999e
admin <rene@rothirsch-tec.at>
2016-12-07 12:48:36 +0100

    Change default language to english

--- wms_backup.sh
+++ wms_backup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 usr=$USER
 prf=1
-imgfol="part_img"
 dbg=0
+
+cd $(dirname $0)
+hdir="$PWD/"
+imgfol="${hdir}part_img"
 
 check_dependencies() {
     dep=("ntp" "ntpdate" "pv")
@@ -10,7 +13,7 @@ check_dependencies() {
     for x in "${dep[@]}"; do
         dpkg-query -W $x &> /dev/null
         if [ $? -eq 1 ]; then
-            echo "$x: ist nicht installiert"
+            echo "$x: is not installed"
             apt-get -y install $x
             ni=1
         fi
@@ -22,23 +25,23 @@ check_dependencies
 # Prüfe ober der ausführende Benutzer root ist.
 if [[ $usr == "root" ]]; then
 
-    echo "wms-one Backup tool"
+    echo "#wms Backup tool"
     echo "Author: René Zingerle"
-    echo "Date: 12.05.2015"
-    echo "Version: 0.08 [BETA]"
-    echo "Infos: http://wmsblog.rothirsch-tec.at/wmsone_backup/index.html"
+    echo "Date: 07.12.2016"
+    echo "Version: 0.09 [BETA]"
+    echo "Info: http://http://wmsblog.rothirsch.tech/wms_backup/"
     echo "---------------------"
 
     echo ""
-    echo "Bitte führen Sie den Datenträger jetzt/erneut in den Card Reader ein!"
-    read -p "Bestätigen Sie mit dem Befehl (mount): " plugged
+    echo "Please insert/reinsert you microSD Card now!"
+    read -p "If the system has recognized the card please write (mount): " plugged
     exit=0
     while [ $exit -eq 0 ]; do
         if [[ $plugged == "mount" ]]; then
             
             # Warte 5 Sekunde
             wait=5
-            echo -n "Warte für 5 Sekunden ["
+            echo -n "Wait for 5 seconds. Check if everything is fine ["
             for ((x=0; x<$wait; x++))
             do
                 echo -n "."
@@ -48,28 +51,27 @@ if [[ $usr == "root" ]]; then
 
             if check_dependencies; then
                
-                echo "Wählen Sie zwischen 2 Optionen:"
-                echo    "[1] Komplett Backup des Datenträgers (Großes Image)" 
-                echo    "[2] Verkleinern der Partitionen für kleinstmögliches Backup (Kleines Image)"
-                read -p "Wählen Sie (1/2): " shrinkdec
+                echo "Choose between following Options: "
+                echo    "[1] Complete Backup of the card. (writes the complete card to the image. The image will have the same size as your card has.)" 
+                echo    "[2] Shrink der ROOTfs partition (backup image will have the size of you ROOTfs)"
+                read -p "Choose (1/2): " shrinkdec
 
                 if [[ $shrinkdec == "1" ]]; then
-                    echo "Befehl akzeptiert, erstelle Vollbackup!"
+                    echo "Command accepted, create Full Backup!"
                 elif [[ $shrinkdec == "2" ]]; then
-                    echo "Befehl akzeptiert, sichere kompletten Datenträger!"
+                    echo "Command accepted, create Small Backup!"
                 fi
 
                 if [[ $shrinkdec == "1" ]] || [[ $shrinkdec == "2" ]]; then
 
                     if [ $prf -eq 1 ]; then
-                    # Erstelle Ordner-Struktur    
-                        mkdir -p tmp
+                    # Create part_img fol
                         mkdir -p ${imgfol}
                     fi
                     
                     if [ $prf -eq 1 ]; then
-                    # Suche der Speichermedien und bereite sie zu einem Auswahlmenü zu
-                        i=0             # Zähler
+                    # Show devices
+                        i=0
                         while read p
                         do
                             if [ $i -ne 0 ]; then
@@ -80,31 +82,30 @@ if [[ $usr == "root" ]]; then
                             fi
                             ((i++))
                         done < <(lsblk -d -o NAME)
-                        read -p "Welchen Datenträger möchten Sie verwenden (Nummer): " ddec
+                        read -p "Choose your backup device [0-9]: " ddec
                         partprobe /dev/${device[$ddec]} 
                     fi
 
                     if [[ $shrinkdec == "2" ]]; then
 
-                        # Suche der Partitionen und deren Größen im Format Byte
+                        # Find partition size in byte
                         if [ $prf -eq 1 ]; then
 
                             # HEAD
-                            i=0                         # @param i: Zähler für while Schleife
-                            part[$i]="Part Array"       # @param part: Array für Partitionsnamen
-                            psize[$i]="Partition Sizes" # @param psize: Array für Partitionsgrößen
+                            i=0
+                            part[$i]="Part Array"       # @param part: Array for partitions
+                            psize[$i]="Partition Sizes" # @param psize: Array for partition size
 
                             # MAIN
                             while read p
                             do
-                            # Durchläuft alle Datenträger
+                            # Runs through all devices
 
                                 if [[ $p == *${device[$ddec]}* ]] && [[ $p != *${device[$ddec]} ]]; then
-                                # Durchläuft nur die Partitionen nicht aber das Gerät selbst
                                     ((i++))
                                     part[$i]=$p
 
-                                    # Entkopple Partitionen vom System wenn diese eingehängt sind
+                                    # Umount mounted partitions
                                     if mountpoint -q /dev/${part[$i]} &> /dev/null; then
                                         echo "${part[$i]} ist eingehängt. Entferne..."
                                         umount /dev/${part[$i]} &> /dev/null
@@ -120,23 +121,22 @@ if [[ $usr == "root" ]]; then
 
 
                         if [ $prf -eq 1 ]; then
-                        # Wähle zu Partition
                             for (( x=0; x<${#part[@]}; x++ ));
                             do
                                 if [ $x -ne 0 ]; then
                                     echo "[$x] ${part[$x]} with ${psize[$x]}"
                                 fi
                             done
-                            read -p "Welche der gewählten Partitionen soll verkleinert werden? (Nummer): " pdec
+                            read -p "Which one is the parition that should be resized? (ROOTfs): " pdec
                         fi
 
-                        # Verkleinern der Systempartition
+                        # Shrint partition of ROOTfs
                         if [ $prf -eq 1 ]; then
 
-                            echo "Überprüfe das Dateisystem..."
+                            echo "Check filesystem..."
                             e2fsck -f /dev/${part[$pdec]}
 
-                            echo "Verkleinere Dateisystem ${part[$pdec]}... "
+                            echo "Shrink filesystem ${part[$pdec]}... "
                             resize2fs -M /dev/${part[$pdec]} 
                             rbc=$(tune2fs -l /dev/${part[$pdec]} | grep "Block count" | tail -1)
                             bsz=$(tune2fs -l /dev/${part[$pdec]} | grep "Block size" | tail -1)
@@ -145,13 +145,13 @@ if [[ $usr == "root" ]]; then
                             ((psizadd=(${rbc}*${bsz})/1000))
                             #read -p "$rbc * $bsz = $psizadd" dec
 
-                            echo "Finde den Startsektor der Partition"
+                            echo "Find start sector of the partition"
                             starsec=$(fdisk -l |grep /dev/${part[$pdec]} | awk '{ print $2 }')
 
-                            echo "Minimiere die Partitionsgröße: ${part[$pdec]}..."
+                            echo "Shrink partition size: ${part[$pdec]}..."
                             (echo d; echo $pdec; echo n; echo p; echo $pdec; echo $starsec ; echo +${psizadd}K; echo w) | fdisk /dev/${device[$ddec]}
 
-                            echo "Überprüfe das Dateisystem..."
+                            echo "Check the filesystem again..."
                             e2fsck -f /dev/${part[$pdec]}
 
                         fi
@@ -160,129 +160,118 @@ if [[ $usr == "root" ]]; then
                     # Sichern der Partitionen
                     if [ $prf -eq 1 ]; then
 
-                        read -p "Möchten Sie das Image in einen Unterordner legen? (y/n): " foldec
-                        i=0
-                        while [ $i -eq 0  ]; do
-                            if [[ $foldec == "y" ]]; then
-                                i=1      
-                                ls -R ${imgfol}/ | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\// /g' -e 's/^/ /'
-                                read -p "Wie soll der Ordner heißen: " bak_fol
-                                NOW=${bak_fol}/$(date +"%Y_%m_%dat%H_%M_%S")
-                            elif [[ $foldec == "n" ]]; then
-                                i=1      
-                                NOW=$(date +"%Y_%m_%dat%H_%M_%S")
-                            else
-                                read -p "Auswahl nicht möglich (y/n)" foldec 
-                            fi
-                        done
+                        ls -R ${imgfol}/ | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\// /g' -e 's/^/ /'
+                        read -p "Name of the backup directory: " bak_fol
+                        NOW=${bak_fol}/$(date +"%Y_%m_%dat%H_%M_%S")
                         mkdir -p ${imgfol}/$NOW
 
                         if [[ $shrinkdec == "2" ]]; then
-                            echo "Sichere die Partitionen..."
+                            echo "Partition backup..."
                             for (( x=0; x<${#part[@]}; x++ ));
                             do
                                 if [ $x -ne 0 ]; then
-                                    echo "Sicher partition ${part[$x]}..."
-                                    echo "Dieser Vorgang kann einige Zeit in Anspruch nehmen!..."
+                                    echo "Backup ${part[$x]}..."
+                                    echo "Please be patient!..."
                                     pv -tpreb /dev/${part[$x]} | dd bs=4M | gzip > ${imgfol}/$NOW/p${x}_wmsone.img.gz && sync
                                 fi
                             done
                             echo "Partition" > ${imgfol}/$NOW/state.txt
                         elif [[ $shrinkdec == "1" ]]; then
-                            echo "Sichere den kompletten Datenträger..."
-                            echo "Dieser Vorgang kann einige Zeit in Anspruch nehmen!..."
+                            echo "Backup the complete filesystem..."
+                            echo "Please be patient..."
                             pv -tpreb /dev/${device[$ddec]} | dd bs=4M | gzip > ${imgfol}/$NOW/complete_wmsone.img.gz && sync
                             echo "Complete" > ${imgfol}/$NOW/state.txt
                         fi
                     fi
 
-                    # Vegrößern des ROOTfs
+                    # Resize des ROOTfs
                     if [ $prf -eq 1 ]; then
-                        echo "Vergrößern des Dateisystems auf Maximum"
-                        echo "Überprüfe das Dateisystem..."
+                        echo "Resize partition /dev/${part[$pdec]} to maximum"
+                        echo "Check the filesystem..."
                         if [ $dbg -eq 0 ]; then 
-                            e2fsck -f /dev/${part[$pdec]} &> /dev/null
+                            e2fsck -f -y -v -C 0 /dev/${part[$pdec]} &> /dev/null
                         else
-                            e2fsck -f /dev/${part[$pdec]}
+                            e2fsck -f -y -v -C 0 /dev/${part[$pdec]}
                         fi
-                        echo "Vergrößere Dateisystem auf maximum..."
+                        echo "Resize filesystem to maximum..."
                         if [ $dbg -eq 0 ]; then 
                             resize2fs -p /dev/${part[$pdec]} &> /dev/null
                         else
                             resize2fs -p /dev/${part[$pdec]}
                         fi
-                        echo "Überprüfe das Dateisystem..."
+                        echo /dev/${part[$pdec]}
+                        echo "Check the filesystem..."
                         if [ $dbg -eq 0 ]; then 
-                            e2fsck -f /dev/${part[$pdec]} &> /dev/null
+                            e2fsck -f -y -v -C 0 /dev/${part[$pdec]} &> /dev/null
                         else
-                            e2fsck -f /dev/${part[$pdec]}
+                            e2fsck -f -y -v -C 0 /dev/${part[$pdec]}
                         fi
                     fi
 
-                    # Beschreiben des Backups
+                    # Write comment 
                     if [ $prf -eq 1 ]; then
 
                         i=0
-                        read -p "Möchten Sie das Backup beschreiben? (y/n): " cdec
+                        read -p "Do you wanna write a comment? (y/n): " cdec
                         while [ $i -eq 0  ]; do
                             if [[ $cdec == "y" ]]; then
-                                read -p "Einzeilige Beschreibung oder mittels vi? (e/v): " edec
+                                read -p "Oneline or vim comment? (o/v): " edec
                                 j=0
                                 while [ $j -eq 0  ]; do
                                     if [[ $edec == "v" ]]; then
                                         vi ${imgfol}/$NOW/comment.txt
                                         j=1
-                                    elif [[ $edec == "e" ]]; then
-                                        read -p "Beschreibung: " comment
+                                    elif [[ $edec == "o" ]]; then
+                                        read -p "Comment: " comment
                                         echo $comment > ${imgfol}/$NOW/comment.txt
                                         j=1
                                     else
-                                        read -p "Auswahl nicht möglich. Wählen Sie (e)inzeilig oder (v)i!: " edec
+                                        read -p "Parameter not allowed. Choose between (o)ne line or (v)i!: " edec
                                         j=0
                                     fi
                                 done
                                 i=1
                             elif [[ $cdec == "n" ]]; then
-                                echo "Keine Beschreibung ausgewählt!"
-                                echo "Keine Beschreibung angegeben!" > ${imgfol}/$NOW/comment.txt
+                                echo "Option: No comment!"
+                                echo "Option: No comment!" > ${imgfol}/$NOW/comment.txt
                                 i=1
                             else
-                                read -p "Auswahl nicht möglich. Wählen Sie (y)es oder (n)o!: " cdec
+                                read -p "Choose between (y)es or (n)o!: " cdec
                                 i=0
                             fi
                         done
 
                         echo ""
-                        echo "Die Partitionen wurden erfolgreich gesichert."
+                        echo "Partition backup successfully ends..."
                             
                     fi
 
-                # Beende den Script bei Falscheingabe
+                # End script on wrong input
                 else
-                    echo "Nur Eingabe von (y)es oder (n)o möglich! Beende Befehl!"         
+                    echo "Choose between (y)es or (n)o"         
                 fi
             
-            # Beende den Script wenn die Abhängigkeiten nicht gegeben sind
+            # End script on missing dependencies
             else
                 echo ""
-                echo "-- ! Bitte installieren Sie die Abhängigkeiten --"
+                echo "-- ! Please install the dependencies --"
             fi
 
         exit=1
-        # Beende den Script wenn das Gerät nicht angehängt ist.
+        # End script if device has no mount state
         else
             echo ""
-            read -p "Eingabe nicht möglich. Geben Sie 'mount' oder 'exit' ein: " plugged
+            read -p "Parameter not allowed. Please use (mount) or (exit): " plugged
         fi
         if [[ $plugged == "exit" ]]; then
             echo ""
-            echo "Script wurde beendet."
+            echo "Script ends."
             exit=1
         fi
     done
 
-# Beende den Script wenn er nicht vom User root ausgeführt wird!
+# End script if not executed by root
 else
-    echo "Der Script muss als Root ausgeführt werden."
+    echo "The script has to be executed as root."
 fi
 
