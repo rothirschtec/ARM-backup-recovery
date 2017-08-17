61ddccf5898851e9ee9d81bf6cf40667e9c50a2f
René Zingerle <rene@rothirsch.tech>
2017-08-17 11:40:25 +0200

    Remove MBR recovery and noconv option

--- wms_recovery.sh
+++ wms_recovery.sh
@@ -319,14 +319,10 @@ if [[ $usr == "root" ]]; then
                                 echo ""
                                 i=1
 
-                                # MBR wiederherstellen
-                                pv -tpreb tmp/mbr_wms.img.gz | dd of=/dev/${device[$ddec]} bs=4M conv=notrunc,noerror && sync
-                                partprobe &> /dev/null
-
                                 for x in tmp/p[0-9]*
                                 do
                                     echo "Write to /dev/${device[$ddec]}p$i..."
-                                    pv -tpreb $x | dd of=/dev/${device[$ddec]}p$i bs=4M conv=notrunc,noerror && sync
+                                    pv -tpreb $x | dd of=/dev/${device[$ddec]}p$i bs=4M && sync
 
                                     if [ $i -gt 1 ]; then
                                         echo "Überprüfe das Dateisystem..."
@@ -339,7 +335,6 @@ if [[ $usr == "root" ]]; then
                                             e2fsck -f /dev/${device[$ddec]}p$i &> /dev/null
                                     fi
 
-                                    partprobe &> /dev/null
                                     (( i++ ))
                                 done
                             fi
